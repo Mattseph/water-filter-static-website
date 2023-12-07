f56d74cbd370d4ba031cea4f69bccc6e2f8c7724
Matthew Joseph Fang Bilaos <105849780+Mattseph@users.noreply.github.com>
2023-12-07 12:08:07 +0800

    Add: Success Page When Email Sent Successfully

---
+++ contact-us/success.php
@@ -0,0 +1,19 @@
+<?php
+$page = 'Success Email Page';
+include './../includes/header.php';
+?>
+
+<body>
+    <div class="card-container">
+        <a href="./../" class="back-to-top d-flex justify-content-center align-items-center"><i class="bi bi-arrow-left-short"></i></a>
+        <div class="card">
+            <div class="card-icon-container">
+                <i class="checkmark">✓</i>
+            </div>
+            <h1>Success</h1>
+            <p>We received your email;<br /> we'll be in touch shortly!</p>
+        </div>
+    </div>
+</body>
+
+</html>
